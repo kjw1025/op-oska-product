5eb8465d692035fb40ff0e2ed805c0b75160f919
unknown <di1025p@gmail.com>
2022-07-06 17:25:46 +0900

    파라솔 bg 작업 및 디테일 수정

--- js/script.js
+++ js/script.js
@@ -1,13 +1,25 @@
 $(document).ready(function () {
 
+
   // 위로가기 기능
   let goTop = $('.gotop');
-  goTop.click(function(){
-    $('html').animate({scrollTop:0}, 1500);
+  let goTopState = 0;
+
+  goTop.click(function (event) {
+    // a 태그이므로 href 를 막아준다.
+    event.preventDefault();    
+    if(goTopState == 0) {
+      // 1번만 실행되도록 flag 를 활용한다.
+      goTopState = 1;
+      $('html').stop().animate({
+        scrollTop: 0
+      }, 1500, function(){
+        // 모션이 완료되어서 도착을 하면
+        // 다시 클릭이 가능하도록 해준다.
+        goTopState = 0;
+      });
+    }
   });
-
-
-
 
 });
 
@@ -23,6 +35,8 @@ window.onload = function () {
   let mainGnb = $('.main-gnb');
   let mainGnbH = mainGnb.height();
 
+  
+
 
   mainGnb.mouseenter(function() {
     header.css('height', mainGnbH);
@@ -30,9 +44,6 @@ window.onload = function () {
   mainGnb.mouseleave(function() {
     header.css('height', 100);
   });
-  // navnFf.mouseenter(function() {
-
-  // });
 
 
 
