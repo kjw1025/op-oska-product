ddd944de0640a43d774386069a8268e3bd22e0fe
unknown <di1025p@gmail.com>
2022-08-05 17:57:28 +0900

    파라솔 섹션 작업

--- js/script.js
+++ js/script.js
@@ -7,13 +7,13 @@ $(document).ready(function () {
 
   goTop.click(function (event) {
     // a 태그이므로 href 를 막아준다.
-    event.preventDefault();    
-    if(goTopState == 0) {
+    event.preventDefault();
+    if (goTopState == 0) {
       // 1번만 실행되도록 flag 를 활용한다.
       goTopState = 1;
       $('html').stop().animate({
         scrollTop: 0
-      }, 1500, function(){
+      }, 1500, function () {
         // 모션이 완료되어서 도착을 하면
         // 다시 클릭이 가능하도록 해준다.
         goTopState = 0;
@@ -35,20 +35,31 @@ window.onload = function () {
   let mainGnb = $('.main-gnb');
   let mainGnbH = mainGnb.height();
 
-  mainGnb.mouseenter(function() {
+  mainGnb.mouseenter(function () {
     header.css('height', mainGnbH);
   });
-  mainGnb.mouseleave(function() {
+  mainGnb.mouseleave(function () {
     header.css('height', 100);
   });
 
 
 
   let sw_parasol = new Swiper('.sw-parasol', {
+    speed: 1500,
     loop: true,
     slidesPerView: 3,
     spaceBetween: 0,
     centeredSlides: true,
+
+    autoplay: {
+      delay: 3000
+    },
+    navigation: {
+      nextEl: ".sw-sol-next",
+      prevEl: ".sw-sol-prev",
+    },
+
+
   });
 
 };
