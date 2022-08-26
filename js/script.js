04ff45d4271e02f675e2dc06e9d2519d34f46a20
= <=>
2022-08-26 14:44:34 +0900

    ani 추가

--- js/script.js
+++ js/script.js
@@ -31,14 +31,68 @@ window.onload = function () {
   let header = $('.header');
   let mainGnb = $('.main-gnb');
   let mainGnbH = mainGnb.height();
+  let logo = $('.logo');
 
   mainGnb.mouseenter(function () {
     header.css('height', mainGnbH);
+    header.css('background-color', 'white');
+    logo.css('background', 'url("../images/logo-gray.svg") no-repeat center');
   });
   mainGnb.mouseleave(function () {
     header.css('height', 100);
+    header.css('background-color', 'transparent');
+    logo.css('background', 'url("../images/logo.svg") no-repeat center');
   });
 
+
+  // wayMotionFn ani
+
+  function wayMotionFn(_what, _effect, _offset, _delay) {
+
+    _what.each(function (index, el) {
+
+      new Waypoint({
+        element: el,
+        handler: function (direction) {
+          var element = $(this.element);
+          setTimeout(function () {
+            element.addClass(_effect);
+          }, _delay);
+          // this.destroy();
+        },
+        offset: _offset
+      });
+  
+    });
+  } 
+
+  // main-txtbox ani
+  wayMotionFn($('.f-main-txtbox > p'), 'fadeup-1', '90%', 0);
+  wayMotionFn($('.f-main-txtbox > strong'), 'fadeup-1', '90%', 250);
+
+  // Pergola set ani
+  wayMotionFn($('.pla-set-imgbox'), 'fadeup-2', '80%', 100);
+  wayMotionFn($('.pla-set-imgbox > a'), 'radius-effect', '80%', 100);
+
+  wayMotionFn($('.pla-set-txtbox > span'), 'fadeup-2', '100%', 0);
+  wayMotionFn($('.pla-set-txtbox > strong'), 'fadeup-2', '100%', 100);
+  wayMotionFn($('.pla-set-txtbox > p'), 'fadeup-2', '100%', 200);
+  wayMotionFn($('.pla-set-txtbox > .plus > a'), 'fadeup-2', '100%', 300);
+
+
+  // parasol set ani
+  wayMotionFn($('.parasol-set > h3'), 'fadeup-2', '60%', 350);
+  wayMotionFn($('.slidepart'), 'fadeup-2', '60%', 300);
+
+  // project ani
+  wayMotionFn($('.project-inner > h3'), 'fadeup-1', '60%', 200);
+  wayMotionFn($('.project-inner > p'), 'fadeup-1', '60%', 250);
+  wayMotionFn($('.project-itembox'), 'fadeup-2', '60%', 250);
+
+  // Contact ani
+  wayMotionFn($('.con-us-txtbox > h3'), 'fadeup-1', '60%', 250);
+  wayMotionFn($('.con-us-txtbox > p'), 'fadeup-1', '60%', 250);
+  wayMotionFn($('.con-us-inner > .more-view-bt'), 'fadeup-1', '60%', 250);
 
 
   let sw_parasol = new Swiper('.sw-parasol', {
@@ -61,17 +115,18 @@ window.onload = function () {
 
   // s and r
   let srBg = $('.s-and-r-bg');
+  let srBg_Active = $('.srBg-r-active');
   let shade = $('.shade');
   let airea = $('.airea');
 
   shade.mouseenter(function () {
-    srBg.css('background', 'url("../images/shade.jpg") no-repeat center');
-    srBg.css('background-size', 'cover');
+    srBg.css('opacity', 1);
+    srBg_Active.css('opacity', 0);
   });
 
   airea.mouseenter(function () {
-    srBg.css('background', 'url("../images/model-airea-1.jpg") no-repeat center');
-    srBg.css('background-size', 'cover');
+    srBg.css('opacity', 0);
+    srBg_Active.css('opacity', 1);
   });
 
   // contact 배경 줌인
@@ -85,7 +140,7 @@ window.onload = function () {
         var element = $(this.element);
         setTimeout(function () {
           element.addClass('scroll-zoomin');
-        }, 0);
+        }, 350);
         // this.destroy();
       },
       offset: '80%'
